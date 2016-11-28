59e9f6466ced5591a2259454888e0356762c41d4
Jessica <11lopez.jessica@gmail.com>
2016-11-28 08:44:29 -1000

    updated with 3 for loops

--- loops_city.js
+++ loops_city.js
@@ -1,1 +1,25 @@
 //Loop City Exercise
+
+//Mommy I can count to 100
+for (var count = 0; count <= 100 ; count++) {
+  console.log("I can count Mommy watch! " + count)
+}
+
+//Noah's Arc
+for (var animalCount = 0; animalCount <= 5; animalCount++) {
+  console.log("There are "+ animalCount + " many animals in Noah's Arc" )
+}
+
+//Hip Hip Array
+var partyAnimals = ["Monkey", "Pig", "Mouse", "Kangeroo", "Fish", "Penguin"];
+
+for (var i in partyAnimals) {
+  console.log(partyAnimals[i] + " are really cool")
+}
+
+//Merlin's Battle Weapon
+
+
+
+
+
