6076efe85cf226c0acd5100d9d1f72a08d78b805
Jessica <11lopez.jessica@gmail.com>
2016-11-30 09:17:38 -1000

    I'm done

--- loops_city.js
+++ loops_city.js
@@ -11,15 +11,31 @@ for (var animalCount = 0; animalCount <= 5; animalCount++) {
 }
 
 //Hip Hip Array
-var partyAnimals = ["Monkey", "Pig", "Mouse", "Kangeroo", "Fish", "Penguin"];
+var partyAnimals = ["Monkeys", "Pigs", "Mouses", "Kangaroos ", "Fishes", "Penguins"];
+var adjective = [" swinging in trees", " eat mud", " tiny", " have a hole in their tummy", " make a blob blob sound", " quacking"]
 
 for (var i in partyAnimals) {
-  console.log(partyAnimals[i] + " are really cool")
+  console.log(partyAnimals[i] + " are" + adjective[i])
 }
 
 //Merlin's Battle Weapon
+var weaponChest = ["Mace", "Sorcerer's Stone", "Exalibur"];
+
+var hero = {
+  wizard : "Merlin",
+  weapon : weaponChest[1],
+  power : 30
+};
+
+function war(weapon) {
+  if(weapon == "Sorcerer's Stone"){
+    console.log(hero.wizard + " shall slay the dragon with the " + weapon);
+  }
+  else{
+    weapon="Sorcerer's Stone";
+    console.log(hero.wizard + " is ready for war with the " + weapon);
+  }
+}
 
 
-
-
-
+war(hero.weapon);
